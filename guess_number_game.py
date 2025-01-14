6b24d14da94699fc7856a5959aa5a2a2febaec44
sigbekuj <sigbekuj@gmail.com>
2025-01-14 02:44:26 +0000

    Update guess_number_game.py

--- guess_number_game.py
+++ guess_number_game.py
@@ -12,4 +12,22 @@ def guess(x):
 
     print(f"Yay, congrats. You have guessed the number {random_number} correctly")
 
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback =" "
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f" Is {guess} too high (H), too low (L), or correct (C)")
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+
+    print("Yay! The computer guessed the number correctly! ")
+
+computer_guess(10)
