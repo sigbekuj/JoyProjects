09735ebb374a678af77c0a07e9e30627f540bbfe
sigbekuj <sigbekuj@gmail.com>
2025-01-14 01:22:49 +0000

    Update madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#string concatenation
+
+name = input("Name: ")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+
+
+madlib = f"Hi my name is {name}! I love Computer programming because its so {adj}. I also love to {verb1} in my spare time and {verb2}. "
+
+print(madlib)
